b7389caec46955ab419c77a35f69a747d31e83a2
nan <ipfilus@gmail.com>
2024-12-18 15:50:53 -0300

    fix: changing wrong pytest markings

--- src/models/sqlite/repositories/pets_repository.py
+++ src/models/sqlite/repositories/pets_repository.py
@@ -1,12 +1,10 @@
 from sqlalchemy.orm.exc import NoResultFound
 from src.models.sqlite.entities.pets import Pets
-import pytest
 
 class PetsRepository:
     def __init__(self, db_connection):
         self.db_connection = db_connection
 
-    @pytest.mark.skip(reason="DB integration test")
     def create(self, pet: Pets) -> Pets:
         with self.db_connection as database:
             try:
@@ -17,7 +15,6 @@ class PetsRepository:
                 database.session.rollback()
                 raise e
 
-    @pytest.mark.skip(reason="DB integration test")
     def list_all(self) -> list:
         with self.db_connection as database:
             try:
@@ -26,7 +23,6 @@ class PetsRepository:
             except NoResultFound:
                 return []
 
-    @pytest.mark.skip(reason="DB integration test")
     def delete(self, pet_id: int) -> None:
         with self.db_connection as database:
             try:
--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,13 +1,16 @@
 from src.models.sqlite.settings.connection import db_connection
 from src.models.sqlite.repositories.pets_repository import PetsRepository
+import pytest
 
 db_connection.connect()
 
+@pytest.mark.skip(reason="DB integration test")
 def test_list_all_pets():
     pets_repository = PetsRepository(db_connection)
     pets = pets_repository.list_all()
     assert len(pets) > 0
 
+@pytest.mark.skip(reason="DB integration test")
 def test_delete_pet():
     pets_repository = PetsRepository(db_connection)
     pets_repository.delete(1)
