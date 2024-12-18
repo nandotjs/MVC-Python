b38597f07abc415d3661e191faf266a3caf96f4e
nan <ipfilus@gmail.com>
2024-12-18 16:25:56 -0300

    feat: add pets-repository test file

---
+++ src/models/sqlite/repositories/pets_repository_test.py
@@ -0,0 +1,24 @@
+from mock_alchemy.mocking import AlchemyMagicMock
+from src.models.sqlite.entities.pets import Pets
+from src.models.sqlite.repositories.pets_repository import PetsRepository
+
+class MockConnection:
+    def __init__(self) -> None:
+        self.session = AlchemyMagicMock()
+        self.session.query(Pets).all.return_value = [
+            Pets(id=1, name="Dog"),
+            Pets(id=2, name="Cat"),
+        ]
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        pass
+
+def test_list_all_pets():
+    mock_connection = MockConnection()
+    pets_repository = PetsRepository(mock_connection)
+    pets = pets_repository.list_all()
+    assert pets[0].id == 1
+    assert pets[1].name == "Cat"
