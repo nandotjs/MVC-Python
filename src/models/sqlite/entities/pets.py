ac69c20aaac5b2ae784047d3cc37f0e46f0bf0de
nan <ipfilus@gmail.com>
2024-12-17 21:16:03 -0300

    feat: add entities

---
+++ src/models/sqlite/entities/pets.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from src.models.sqlite.settings.base import Base
+
+class Pets(Base):
+    __tablename__ = "pets"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"Pets(id={self.id}, name={self.name}, type={self.type})"
