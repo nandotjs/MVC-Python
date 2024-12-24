c9a5a78fa58cd069e9fc70bd7f3d82691af162cc
nan <ipfilus@gmail.com>
2024-12-23 23:31:58 -0300

    feat: add create owner controller

--- src/controllers/owner_creator_controller.py
+++ src/controllers/owner_creator_controller.py
@@ -1,8 +1,33 @@
 from src.models.sqlite.interfaces.owners_repository import OwnersRepositoryInterface
+import re
 
 class OwnerCreatorController:
     def __init__(self, owners_repository: OwnersRepositoryInterface):
         self.owners_repository = owners_repository
 
-    def create(self, first_name: str, last_name: str, age: int, pet_id: int):
+    def create(self, owner_info: dict) -> dict:
+        first_name = owner_info["first_name"]
+        last_name = owner_info["last_name"]
+        age = owner_info["age"]
+        pet_id = owner_info["pet_id"]
+
+        self.__validate_names(first_name, last_name)
+        self.__insert_owner(first_name, last_name, age, pet_id)
+        return self.__format_response(owner_info)
+
+    def __validate_names(self, first_name: str, last_name: str) -> None:
+        non_valid_caracters = re.compile(r'[^a-zA-Z]')
+        if non_valid_caracters.search(first_name) or non_valid_caracters.search(last_name):
+            raise ValueError("First name and last name must contain only letters")
+
+    def __insert_owner(self, first_name: str, last_name: str, age: int, pet_id: int) -> None:
         self.owners_repository.create(first_name, last_name, age, pet_id)
+
+    def __format_response(self, owner_info: dict) -> dict:
+        return {
+            "data": {
+                "type": "owner",
+                "id": owner_info["id"],
+                "attributes": owner_info
+            }
+        }
